05e37aee476bc07346599743f249e5497d94b32b
HueNguyen <nguyenthihue1301tb@hotmail.com>
2021-06-19 16:33:19 +0700

    update

--- app/src/main/java/com/example/hpl/active_home.java
+++ app/src/main/java/com/example/hpl/active_home.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class active_home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_active_home);
     }
 }
---
+++ app/src/main/java/com/example/hpl/activity_questionwrong.java
@@ -0,0 +1,14 @@
+package com.example.hpl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_questionwrong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionwrong);
+    }
+}
